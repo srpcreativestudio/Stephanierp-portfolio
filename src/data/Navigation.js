b1b9727d4a9c1186eb9f55a2567877f8849c7c8a
stephanierpatton <stephanie@stephrpatton.com>
2025-05-06 20:58:29 -0500

    update footer, fix nav error

--- src/data/Navigation.js
+++ src/data/Navigation.js
@@ -1,14 +1,12 @@
-const mainNavItems = [
+export const mainNavItems = [
   { href: "#home", label: "Home" },
   { href: "#portfolio", label: "Portfolio" },
   { href: "#about", label: "About" },
   { href: "#", label: "Contact", modalType: "contact" }
 ];
-const footerNavItems = [
+export const footerNavItems = [
   { href: "#portfolio", label: "Portfolio" },
   { href: "#about", label: "About" },
   { href: "#", label: "Contact", modalType: "contact" },
   { href: "#", label: "Privacy Policy", modalType: "privacy" }
 ];
-
-export { mainNavItems, footerNavItems };
