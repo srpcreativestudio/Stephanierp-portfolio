0662aaed25c1fc7f68ab8b8427181dab2ffba9a4
stephanierpatton <stephanie@stephrpatton.com>
2025-05-06 21:07:38 -0500

    fix nav error deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import lottie from "astro-integration-lottie";
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-         '@data': '/src/data',
-      }
-    }
-  },
   integrations: [
     lottie(),
-  ]
+    tailwind()
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        '@data': '/src/data'
+      }
+    },
+    build: {
+      rollupOptions: {
+        external: ['../data/navigation.js']
+      }
+    }
+  }
 });
